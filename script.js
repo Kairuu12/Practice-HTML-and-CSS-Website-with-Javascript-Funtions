831492a21494ffe2ee0b5416ed61cbd3fb833f5d
Kairuu12 <akokyle10@gmail.com>
2025-03-01 15:19:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function Submit() {
-    let x = document.getElementById("fname").value;
-    let y = document.getElementById("lname").value;
-    let z = ("Hello " + x + " " + y + " ! " + "Welcome to my Practice Website!");
-    document.getElementById("name-box").innerHTML = z;
+    const firstName = document.getElementById("fname").value.trim();
+    const lastName = document.getElementById("lname").value.trim();
+
+    if (!firstName || !lastName) {
+        alert("Please fill in all fields");
+    } else {
+        const fullName = ("Hello " + firstName + " " + lastName + " and welcome to my practice website!");
+        document.getElementById("name-box").innerHTML = fullName;
+    }
 }
